db743de91e77a27f85f3986d2f613e18cc0b9e22
srikanthmaturu <srikanthmaturu@outlook.com>
2017-09-01 17:25:28 -0500

    np_max increased

--- src/tf_idf_index/index.cpp
+++ src/tf_idf_index/index.cpp
@@ -162,7 +162,7 @@ int main(int argc, char* argv[]){
                         np_max = l * 3;
                     }
                     else {
-                        np_max = l * 6;
+                        np_max = l * 12;
                     }
                     for(uint64_t np = l; np < np_max; np +=5){
                         tf_idf_falconn_i.updateParmeters(l, nhb, np);
