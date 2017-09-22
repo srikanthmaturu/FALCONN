bf9a934d36e13be0680266f6b6b901e371edeec1
Srikanth Maturu <srikanthmaturu92@gmail.com>
2017-09-22 13:51:47 -0500

    fix

--- src/tf_idf_index/tf_idf_falconn_idx.hpp
+++ src/tf_idf_index/tf_idf_falconn_idx.hpp
@@ -353,7 +353,7 @@ namespace tf_idf_falconn_index {
                 auto pure_cosine_angle = acos(pure_cosine_distance)/3.14;
                 auto pure_euclidean_distance = (data_item_pure_tf_idf_vector - query_pure_tf_idf_vector).squaredNorm();
 
-                string result =  to_string(uiLevenshteinDistance(query, original_data[i])) + "," + to_string(pure_cosine_distance) + "," + to_string(cosine_distance) + "," + to_string(pure_cosine_angle) + "," + to_string(cosine_angle) + "," + to_string(euclidean_distance) + "," + to_string(pure_euclidean_distance) + "\n";
+                string result =  to_string(uiLevenshteinDistance(query, original_data[i])) + "," + to_string(pure_cosine_distance) + "," + to_string(cosine_distance) + "," + to_string(pure_cosine_angle) + "," + to_string(cosine_angle) + "," + to_string(pure_euclidean_distance) + "," + to_string(euclidean_distance) + "\n";
                 results[i] = result;
             }
 
