f935a71274b4e34f38a119f4eb87c6c0abd0592c
Srikanth Maturu <srikanthmaturu92@gmail.com>
2017-09-24 18:31:30 -0500

    fix

--- src/tf_idf_index/index.cpp
+++ src/tf_idf_index/index.cpp
@@ -79,13 +79,13 @@ void load_sequences(string sequences_file, vector<string>& sequences){
 }
 
 template<class index_type>
-double process_queries_parallely(index_type& tf_idf_falconn_i, vector<string>& queries, uint64_t number_of_blocks, uint64_t block_size){
+double process_queries_parallely(index_type& tf_idf_falconn_i, vector<string>& queries, uint64_t number_of_blocks, uint64_t block_size, vector<vector<string>> linear_results){
     uint64_t realMatchesCount = 0, actualMatchesCount = 0, queries_size = queries.size();
 //#pragma omp parallel for
     for(uint64_t bi = 0; bi < number_of_blocks; bi++){
         uint64_t block_end = (bi == (number_of_blocks-1))? queries_size : (bi + 1)*block_size;
         for(uint64_t i= bi * block_size, j = 0; i< block_end; i++, j++){
-            auto linear_res = tf_idf_falconn_i.get_nearest_neighbours_by_linear_method(queries[i], 30);
+            vector<string>& linear_res = linear_results[i];
             auto res = tf_idf_falconn_i.match(queries[i]);
             auto cs_fp_fn_pair = get_comparison(linear_res, res.second);
             realMatchesCount += (linear_res.size());
@@ -99,14 +99,30 @@ double process_queries_parallely(index_type& tf_idf_falconn_i, vector<string>& q
 template<class index_type>
 void process_queries_box_test(index_type& tf_idf_falconn_i, vector<string>& queries){
     ofstream box_test_results_file("box_test_results_NGL_" + to_string(NGRAM_LENGTH) + ".csv");
-    vector< vector< pair<string, uint64_t > > > query_results_vector;
+
     uint64_t block_size = 100000;
     uint64_t queries_size = queries.size();
     std::cout << queries_size << std::endl;
     if(queries_size < block_size){
         block_size = queries_size;
     }
+    uint64_t extra_block = queries_size % block_size;
+    uint64_t number_of_blocks =  queries_size / block_size;
+    auto start = timer::now();
+    if(extra_block > 0) {
+        number_of_blocks++;
+    }
+
     vector<vector<uint64_t>> queries_linear_results(queries.size(),vector<uint64_t>(3,0));
+    vector<vector<string>> linear_results;
+
+    for(uint64_t bi = 0; bi < number_of_blocks; bi++){
+        uint64_t block_end = (bi == (number_of_blocks-1))? queries_size : (bi + 1)*block_size;
+        for(uint64_t i= bi * block_size, j = 0; i< block_end; i++, j++){
+            linear_results.push_back(tf_idf_falconn_i.get_nearest_neighbours_by_linear_method(queries[i], 30));
+        }
+    }
+
     uint64_t polytope_vertices = NGRAM_LENGTH * 2 + 1;
 
     for(uint64_t l = 32; l <= 64; l += 32){
@@ -121,18 +137,11 @@ void process_queries_box_test(index_type& tf_idf_falconn_i, vector<string>& quer
             uint64_t step = 1000;
             for(uint64_t np = l; np < np_max; np = np + step){
                 tf_idf_falconn_i.updateParmeters(l, nhb, np);
-                vector< vector< pair<string, uint64_t > > > query_results_vector;
                 cout << "Current LSH Parameters: " << endl;
                 tf_idf_falconn_i.printLSHConstructionParameters();
                 tf_idf_falconn_i.construct_table();
-                uint64_t extra_block = queries_size % block_size;
-                uint64_t number_of_blocks =  queries_size / block_size;
-                auto start = timer::now();
-                if(extra_block > 0) {
-                    number_of_blocks++;
-                }
 
-                double recall = process_queries_parallely(tf_idf_falconn_i, queries, number_of_blocks, block_size);
+                double recall = process_queries_parallely(tf_idf_falconn_i, queries, number_of_blocks, block_size, linear_results);
                 auto stop = timer::now();
                 box_test_results_file << recall << "," << (duration_cast<chrono::microseconds>(stop-start).count()/1000000.0)/(double)queries.size() << "," << l << "," << nhb << "," << np << endl;
                 if(np_max <= (np + step) && (recall < 0.95)){
